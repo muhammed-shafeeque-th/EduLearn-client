b7ced5e37331a43d18ebe8cc483772fb974b3e2c
shafeeque <shafeeqsha06@gmail.com>
2026-02-15 11:31:22 +0530

    feat(revenue): add instructor revenue report export(TODO) component

---
+++ src/app/instructor/revenue/_/components/export-report.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React from 'react';
+import { Download, FileText, FileSpreadsheet } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export const ExportReports: React.FC = () => {
+  const handleExport = (format: string) => {
+    // Server action would go here
+    console.log('Exporting as:', format);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <Download className="h-4 w-4 mr-2" />
+          Export Reports
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel>Export Format</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => handleExport('pdf')}>
+          <FileText className="h-4 w-4 mr-2" />
+          Export as PDF
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleExport('csv')}>
+          <FileSpreadsheet className="h-4 w-4 mr-2" />
+          Export as CSV
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleExport('excel')}>
+          <FileSpreadsheet className="h-4 w-4 mr-2" />
+          Export as Excel
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
