1ce9757cd90490db3bd0a54ec5a5c45d1ece0dd5
shafeeque <shafeeqsha06@gmail.com>
2026-02-15 22:58:17 +0530

    chore: update eslint config and next config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,23 +7,42 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: {},
-  allConfig: {},
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '*.config.js',
+      '.next/',
+      'public',
+      '**/__*/**',
+      '**/**/_*.ts*',
+      '**/*.json',
+    ], // Ignore specific files and directories
+  },
   ...compat.extends(
     'next/core-web-vitals',
     'next',
     // 'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended'
   ),
   {
     settings: {
       react: {
         version: 'detect',
       },
+    },
+    rules: {
+      'no-trailing-spaces': 'off',
+      'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+      'react/prop-types': 'off', // Disable prop-types rule for React
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Ignore unused variables starting with _
+      'prettier/prettier': ['error', { endOfLine: 'auto' }], // Ensure consistent line endings
+      // If you want to disallow the use of the `object` type in TypeScript, use:
     },
   },
 ];
