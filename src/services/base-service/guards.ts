70495576a0c501be7db21ec63b47f80af64f8af1
shafeeque <shafeeqsha06@gmail.com>
2026-03-06 23:25:34 +0530

    feat(base-api): update base axios api service to emit a custom logout event if response status is 403

--- src/services/base-service/guards.ts
+++ src/services/base-service/guards.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from 'axios';
 import { RequestOptions } from './types';
+import { AppError, ErrorDetail } from '@/lib/errors/app-error';
 
 export function isAxiosError(error: unknown): error is AxiosError {
   return typeof error === 'object' && error !== null && (error as AxiosError).isAxiosError === true;
@@ -14,6 +15,55 @@ export function hasErrorMessage(obj: unknown): obj is { error: { message: string
     (obj as { error: { message?: unknown } }).error !== null &&
     'message' in (obj as { error: { message?: unknown } }).error &&
     typeof (obj as { error: { message?: unknown } }).error.message === 'string'
+  );
+}
+export function hasErrorWithDetails(
+  obj: unknown
+): obj is { error: { details: ErrorDetail[]; message: string } } {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'error' in obj &&
+    typeof (obj as { error?: unknown }).error === 'object' &&
+    (obj as { error: { message?: unknown } }).error !== null &&
+    'message' in (obj as { error: { message?: unknown } }).error &&
+    typeof (obj as { error: { message?: unknown } }).error.message === 'string' &&
+    'details' in (obj as { error: { details?: unknown } }).error &&
+    Array.isArray((obj as { error: { details?: unknown } }).error.details)
+    //  && (obj as { error: { details?: unknown } }).error.details.every((detail) => {
+    //   return (
+    //     typeof detail === 'object' &&
+    //     detail !== null &&
+    //     'field' in detail &&
+    //     typeof (detail as { field?: unknown }).field === 'string' &&
+    //     'message' in detail &&
+    //     typeof (detail as { message?: unknown }).message === 'string'
+    //   );
+    // })
+  );
+}
+export function isAppError(obj: unknown): obj is AppError {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'errorCode' in obj &&
+    typeof (obj as { errorCode?: unknown }).errorCode === 'string' &&
+    'message' in obj &&
+    typeof (obj as { message?: unknown }).message === 'string' &&
+    'details' in obj &&
+    Array.isArray((obj as { details?: unknown }).details) &&
+    'status' in obj &&
+    typeof (obj as { status?: unknown }).status === 'number'
+    //  && (obj as { error: { details?: unknown } }).error.details.every((detail) => {
+    //   return (
+    //     typeof detail === 'object' &&
+    //     detail !== null &&
+    //     'field' in detail &&
+    //     typeof (detail as { field?: unknown }).field === 'string' &&
+    //     'message' in detail &&
+    //     typeof (detail as { message?: unknown }).message === 'string'
+    //   );
+    // })
   );
 }
 
