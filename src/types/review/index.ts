3102d71105f52ec2eed4b62376d885e083e0e69d
shafeeque <shafeeqsha06@gmail.com>
2026-03-07 14:18:50 +0530

    feat(review): update review domain type

--- src/types/review/index.ts
+++ src/types/review/index.ts
@@ -3,15 +3,20 @@ export interface Review {
   id: string;
   courseId: string;
   enrollmentId: string;
-  course: Course;
+  course?: Course;
   userId: string;
   user?: {
     name: string;
     avatar: string;
     id: string;
   };
+  name?: string; // name for UI
+  avatar?: string; // avatar for UI
   rating: number;
   comment: string;
+  verified?: boolean;
+  courseName?: string;
+  helpfulCount: number;
   createdAt: string;
   updatedAt: string;
 }
