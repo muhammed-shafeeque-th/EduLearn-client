f9ffab310885f623f0fa05b20cd8b84fa55c9440
shafeeque <shafeeqsha06@gmail.com>
2026-03-07 12:54:52 +0530

    feat(langing): add landing page components testimonial-section , loading-skeletons and instructor-section

---
+++ src/app/(common)/_/components/landing/index.tsx
@@ -0,0 +1,65 @@
+import { Suspense } from 'react';
+
+// Server Components
+import StatsSection from './components/stats-section';
+import CategoriesSection from './components/categories-section';
+
+// Loading Components
+import {
+  HeroSkeleton,
+  StatsSkeleton,
+  CategoriesSkeleton,
+  CoursesSkeleton,
+  InstructorsSkeleton,
+  // TestimonialsSkeleton,
+  // CTASkeleton,
+} from './components/loading-skeletons';
+import CoursesSection from './components/course-section-cl';
+import InstructorsSection from './components/instructors-section-cl';
+import TestimonialsSection from './components/testimonial-section';
+import { CTASection } from './components/call-to-action-section';
+import HeroSection from './components/hero-section-cl';
+
+export default function LandingPage() {
+  return (
+    <main className="min-h-screen">
+      <Suspense fallback={<HeroSkeleton />}>
+        <HeroSection />
+      </Suspense>
+
+      <Suspense fallback={<StatsSkeleton />}>
+        <StatsSection />
+      </Suspense>
+
+      <Suspense fallback={<CategoriesSkeleton />}>
+        <CategoriesSection />
+      </Suspense>
+
+      <Suspense fallback={<CoursesSkeleton />}>
+        <CoursesSection />
+      </Suspense>
+
+      <Suspense fallback={<InstructorsSkeleton />}>
+        <InstructorsSection />
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <div>Loading...</div>
+          // <TestimonialsSkeleton />
+        }
+      >
+        <TestimonialsSection />
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <div>Loading...</div>
+          // <CTASkeleton />
+        }
+      >
+        <CTASection />
+      </Suspense>
+    </main>
+  );
+}
