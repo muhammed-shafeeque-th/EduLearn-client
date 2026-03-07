fc0b7737b54ca2ac1370bd838ecb2cb8c222fca6
shafeeque <shafeeqsha06@gmail.com>
2026-03-07 11:05:07 +0530

    feat: add app level constants

---
+++ src/lib/constants/cookie-tokens.ts
@@ -0,0 +1,3 @@
+export const authCookieToken = '__edulearn_auth_token';
+export const adminAuthToken = '__edulearn_admin_token';
+export const authRefreshToken = '__edulearn_refresh-token';
---
+++ src/lib/constants/storage-keys.ts
@@ -0,0 +1,2 @@
+export const authLocalStoreKey = '__edulearn_storage_auth_key';
+export const adminLocalStoreKey = '__edulearn_storage_admin_key';
