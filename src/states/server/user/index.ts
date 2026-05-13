d51f5bd129ae6d22177262b8f8d0eeab9ebabeb8
shafeeque <shafeeqsha06@gmail.com>
2026-05-13 22:21:14 +0530

    feat(user): add react query user hook barrel file

---
+++ src/states/server/user/index.ts
@@ -0,0 +1,5 @@
+export * from './use-current-user';
+export * from './use-instructors';
+export * from './use-update-user';
+export * from './use-user-stats';
+export { useInstructors, useUser, useUsers, useUsersInfinite } from './use-users';
