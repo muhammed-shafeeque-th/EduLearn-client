eef4be6ab85a1749e9d18629ee2f477d9a71c1a6
shafeeque <shafeeqsha06@gmail.com>
2026-08-20 15:02:55 +0530

    feat: update root provider to use the updated redux store

--- src/lib/providers/root-providers.tsx
+++ src/lib/providers/root-providers.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, ReactNode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider as ReduxProvider } from 'react-redux';
-import { getStore } from '@/states/client';
+import { getStore } from '@/states/client/store';
 import { restoreCredentials, logout, refreshToken } from '@/states/client/slices/auth-slice';
 import { AuthCustomEvents } from '../constants/auth-events';
 import { createAppQueryClient } from './query-client-config';
