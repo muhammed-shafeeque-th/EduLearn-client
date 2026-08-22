fbf90005fbfbc8005ea2eb5a721e66601b938cf6
shafeeque <shafeeqsha06@gmail.com>
2026-08-22 17:09:51 +0530

    fix: remove auth unnessosory auth check, which already done by the middleware

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react';
 import { Metadata } from 'next';
 import { RouteFallback } from '@/components/ui/route-fallback';
-import { authGuard } from '@/lib/auth';
-import { redirect } from 'next/navigation';
+// import { authGuard } from '@/lib/auth';
+// import { redirect } from 'next/navigation';
 import SiteFooter from '@/components/layout/site-footer';
 import { SiteHeader } from '@/components/layout/site-header';
 
@@ -12,15 +12,15 @@ export const metadata: Metadata = {
 };
 
 export default async function AuthLayout({ children }: { children: React.ReactNode }) {
-  await authGuard({
-    condition: (user) => !!user?.id,
-    onUnauthorized: () => {
-      redirect('/');
-    },
-    redirectOnException: false,
-    // If user not authenticated return null do nothing
-    returnNullInsteadOfRedirect: true,
-  });
+  // await authGuard({
+  //   condition: (user) => !!user?.id,
+  //   onUnauthorized: () => {
+  //     redirect('/');
+  //   },
+  //   redirectOnException: false,
+  //   // If user not authenticated return null do nothing
+  //   returnNullInsteadOfRedirect: true,
+  // });
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900">
